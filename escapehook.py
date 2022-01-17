34173f82bfa28e5784adf09cc0fe977a2d6d48a3
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-01-17 23:29:14 +0530

    fix section links

--- escapehook.py
+++ escapehook.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import urllib.parse
 import re
 import sys
 
@@ -39,7 +40,7 @@ for i in range(1, len(linkrefs)-2):
         newtitle = inhtml
     linkrefs[i] = linkrefs[i][:titleloc[0]]+"\""+newtitle+"\""
     parts = re.match("\[(.*)\]:\ (.*?)\ \"(.*)\"", linkrefs[i]).groups()
-    link = parts[0].split("|")[0]
+    link = parts[0].split("|")[0].replace(" ", "-").lower()
     linkrefs[i] = f'[{parts[0]}]: {link} "{parts[2]}"'
     
 open(sys.argv[-1], "w").write(text[:linkrefstart]+"\n".join(linkrefs))
