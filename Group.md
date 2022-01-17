9c677d110b27765879c997792e366ecd54efc43e
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-01-17 19:59:01 +0530

    fix escape hook.py

--- Group.md
+++ Group.md
@@ -1,8 +1,8 @@
 # Group
 
-G = $(\{S\}, *)$
+G = $(\{S\}, \*)$
 
-1. $S$ is closed over $*$
-2. $*$ is associative. $a(bc) = (ab)c = abc$
+1. $S$ is closed over $\*$
+2. $\*$ is associative. $a(bc) = (ab)c = abc$
 3. Identity exists
 4. Inverse exists
--- escapehook.py
+++ escapehook.py
@@ -7,11 +7,13 @@ print(sys.argv[-1])
 text = open(sys.argv[-1], encoding="utf8").read()
 list_text = list(text)
 
-mathsections = [r.span() for r in re.finditer(r"\$(.\*)\$", text)]
+mathsections = [r.span() for r in re.finditer(r"\$.*\$", text)]
+print("Found", len(mathsections), "math sections")
 
 for section in mathsections:
     for i in range(*section):
         if list_text[i] in ["*", "|"] and list_text[i-1] != "\\":
+            print("Replacing", list_text[i], "at", i)
             list_text[i] = "\\"+list_text[i]
 
 open(sys.argv[-1], "w").write("".join(list_text))
